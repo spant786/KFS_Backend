634d1b5ecbd4de6258b19027f49c4d1b7ba299d3
Sagar Pant <spant786@gmail.com>
2020-01-26 20:15:37 +0545

    router for product added

---
+++ router/product.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const Product = require('../models/product')
+const router = new express.Router()
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+//
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
+
+router.post("/product",upload.single('Image'),(req,res)=>{
+    //console.log(req.body)
+   var myData = new Product(req.body);
+   myData.save();
+});
+//get ko lagi code
+router.get('/getProduct',function(req,res){
+    Product.find().then(function(user_data){
+        res.send(user_data);
+
+    
+}).catch(function(e){
+    
+            res.send(e)
+        
+    });
+})
+//yaha sama  get ko code 
+
+
+//yaha bata taltira delete ko 
+router.delete('/delProduct/:id',function(req,res){
+    Product.findByIdAndDelete(req.params.id).then(function(){
+
+    }).catch(function(){
+        res.send(e)
+    })
+
+    
+});
+
+router.put('/updateProduct/:id',function(req,res){
+    Product.findOneAndUpdate({_id :req.params.id},req.body).then(function(){
+        res.send("updated")
+    }).catch(function(e){
+        res.send(e)
+    })
+})
+
+module.exports = router
